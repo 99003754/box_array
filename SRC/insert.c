eee58ffa2fcbc058f21d36855aec8b84059b48cb
99003754 <78854545+99003754@users.noreply.github.com>
2021-02-22 21:20:29 +0530

    Update insert.c

--- SRC/insert.c
+++ SRC/insert.c
@@ -1,15 +1,15 @@
 #include"main.h"
-int insert(struct box *b2,int increment)
+int insert(struct box *b2,int increment,int update_id,float update_length,float upate_breadth,float update_height,float update_weight,char update_color[20])
 {
  int n,i;
 printf("1");
  b2++;
- b2->unique_id=1;
- b2->length=1;
- b2->breadth=1;
- b2->height=1;
- b2->weight=1;
- strcpy(b2->color,"abcdefgh");
+ b2->unique_id=update_id;
+ b2->length=update_length;
+ b2->breadth=update_breadth;
+ b2->height=update_height;
+ b2->weight=update_weight;
+ strcpy(b2->color,update_color);
  increment++;
  return increment;
 }
