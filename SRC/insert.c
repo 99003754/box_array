59601696958e7df1dbd2eb4596fa90eaa68d951e
99003754 <78854545+99003754@users.noreply.github.com>
2021-02-22 21:46:31 +0530

    Update insert.c

--- SRC/insert.c
+++ SRC/insert.c
@@ -1,5 +1,5 @@
 #include"main.h"
-int insert(struct box *b2,int increment,int update_id,float update_length,float upate_breadth,float update_height,float update_weight,char update_color[20])
+int insert(struct box *b2,int increment,int update_id,float update_length,float update_breadth,float update_height,float update_weight,char update_color[20])
 {
  if(increment==0)
  {
