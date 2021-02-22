b08415995bda87a73c707d1b4e54482f7474ebf5
99003754 <78854545+99003754@users.noreply.github.com>
2021-02-22 21:52:56 +0530

    Update main.h

--- SRC/main.h
+++ SRC/main.h
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<string.h>
- int display(struct box *b2,int x);
+ //int display(struct box *b2,int x);
 int insert(struct box *b2,int increment,int update_id,float update_length,float update_breadth,float update_height,float update_weight,char update_color[20]);
- int search(struct box *b2,int x,int search_id);
+ //int search(struct box *b2,int x,int search_id);
  struct box{
  int unique_id;
  float length,breadth,height,weight;
