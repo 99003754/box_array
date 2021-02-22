257a02f61c9f009059d0f9410e2c3e40fc428fe5
99003754 <78854545+99003754@users.noreply.github.com>
2021-02-22 16:37:44 +0530

    Added Test

---
+++ SRC/test.c
@@ -0,0 +1,23 @@
+#include"main.h"
+int main()
+{
+int i;
+
+
+ struct box b1[10];
+ struct box *ptr= NULL ;
+ ptr=(struct box*)malloc(10*sizeof(struct box));
+ ptr=b1;
+ ptr->unique_id=1;
+ ptr->length=1;
+ ptr->breadth=1;
+ ptr->height=1;
+ ptr->weight=1;
+ strcpy(ptr->color,"abcd");
+ 
+ int x=insert(&b1);
+ display(&b1,x);
+ 
+ 
+
+ }
