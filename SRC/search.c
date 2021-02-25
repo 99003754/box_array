ceb489f1c23285a62640726dcb382e845e8f1fdd
99003746 <nitin.shetty1@ltts.com>
2021-02-25 10:05:07 +0530

    logic for search() changed

--- SRC/search.c
+++ SRC/search.c
@@ -10,12 +10,12 @@
          printf("i= %d id= %d\n",i,b2->unique_id);
          if(search_id==b2->unique_id)
          {
-             //printf("\ngiven  id %d ");
-             //printf("Length =%d",b2->length );
-             //printf("breadth =%d",b2->breadth );
-             //printf("Weight =%d",b2->weight );
-             //printf("Height =%d",b2->height );
-             //printf("Colour=%s",b2->color );
+             printf("\ngiven  id %d ");
+             printf("Length =%d",b2->length );
+             printf("breadth =%d",b2->breadth );
+             printf("Weight =%d",b2->weight );
+             printf("Height =%d",b2->height );
+             printf("Colour=%s",b2->color );
              return 1;
 
          }
