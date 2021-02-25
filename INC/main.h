cdee19911d107537547a0706ff4a6ee3809840ce
99003770 <78854076+99003770@users.noreply.github.com>
2021-02-25 15:50:10 +0530

    Update main.h

--- INC/main.h
+++ INC/main.h
@@ -1,17 +1,23 @@
-
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-//extern int increment;
 
+ //int display(struct box *b2,int x);
+//int insert(struct box *b2,int increment,int update_id,float update_length,float update_breadth,float update_height,float update_weight,char update_color[20]);
+ //int search(struct box *b2,int x,int search_id);
+ //float max_height(struct box *ptr);
  struct box{
  int unique_id;
  float length,breadth,height,weight;
- int color;
+ char color[20];
 };
-
-int insert(struct box *b2,int uid,float update_length,float update_breadth,float update_height,float update_weight,int update_color);
-int search(struct box *b2,int search_id);
-
-
-//
+//int increment=0;
+/**
+ * C program to delete an element from array at specified position
+ * Input size and element in array
+ * Input size and element in array
+ * Invalid delete position
+ * Copy next element value to current element
+ *  Decrement array size by 1
+ * Print array after deletion
+*/
